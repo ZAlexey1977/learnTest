3176ef4270f9e2cc3b5f6bc2dff1a6f2e1c06935
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-10-14 18:18:47 +0300

    unit 1

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 let log = console.log;
+
+//////////////////////////// unit 1 ///////////////////////////////
+
+// function palindrome(str) {
+//   str = str.toLowerCase();
+//   let myStr = str.split(/[\W_]/);
+//   myStr = myStr.join('');
+//   myStr = myStr.split('');
+//   myStr = myStr.join('');
+//   let newStr = str.split(/[\W_]/);
+//   newStr = newStr.join('');
+//   newStr = newStr.split('');
+//   newStr = newStr.reverse();
+//   newStr = newStr.join('');
+//   log(newStr);
+//   log(myStr);
+//   if (myStr !== newStr) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// log(palindrome("A man, a plan, a canal. Panama"));
+
+/////////////////////////// unit 2 ///////////////////////////////
+
+/////////////////////////// unit 3 ///////////////////////////////
+
+/////////////////////////// unit 4 ///////////////////////////////
+
+/////////////////////////// unit 5 ///////////////////////////////
+
+/////////////////////////// unit 6 ///////////////////////////////
