b15ed93aac75efd02ee3f3f0b0e66263594940be
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-10-16 18:00:44 +0300

    unit 3

--- script.js
+++ script.js
@@ -25,139 +25,157 @@ let log = console.log;
 
 /////////////////////////// unit 2 ///////////////////////////////
 
-let numbers = {
-  1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
-}
+// function convertToRoman(num) {
+//   num = String(num);
+//   let result = [];
 
-function convertToRoman(num) {
-  num = String(num);
-  let result = [];
+//   let myNum = num.split('').reverse();
+//   log(myNum)
+//   switch (myNum[0]) {
+//     case '0':
+//       result.unshift('');
+//       break;
+//     case '1':
+//       result.unshift('I');
+//       break;
+//     case '2':
+//       result.unshift('II');
+//       break;
+//     case '3':
+//       result.unshift('III');
+//       break;
+//     case '4':
+//       result.unshift('IV');
+//       break;
+//     case '5':
+//       result.unshift('V');
+//       break;
+//     case '6':
+//       result.unshift('VI');
+//       break;
+//     case '7':
+//       result.unshift('VII');
+//       break;
+//     case '8':
+//       result.unshift('VIII');
+//       break;
+//     case '9':
+//       result.unshift('IX');
+//       break;
+//   }
 
-  let myNum = num.split('').reverse();
-  log(myNum)
-  switch (myNum[0]) {
-    case '0':
-      result.unshift('');
-      break;
-    case '1':
-      result.unshift('I');
-      break;
-    case '2':
-      result.unshift('II');
-      break;
-    case '3':
-      result.unshift('III');
-      break;
-    case '4':
-      result.unshift('IV');
-      break;
-    case '5':
-      result.unshift('V');
-      break;
-    case '6':
-      result.unshift('VI');
-      break;
-    case '7':
-      result.unshift('VII');
-      break;
-    case '8':
-      result.unshift('VIII');
-      break;
-    case '9':
-      result.unshift('IX');
-      break;
-  }
+//   switch (myNum[1]) {
+//     case '0':
+//       result.unshift('');
+//       break;
+//     case '1':
+//       result.unshift('X');
+//       break;
+//     case '2':
+//       result.unshift('XX');
+//       break;
+//     case '3':
+//       result.unshift('XXX');
+//       break;
+//     case '4':
+//       result.unshift('XL');
+//       break;
+//     case '5':
+//       result.unshift('L');
+//       break;
+//     case '6':
+//       result.unshift('LX');
+//       break;
+//     case '7':
+//       result.unshift('LXX');
+//       break;
+//     case '8':
+//       result.unshift('LXXX');
+//       break;
+//     case '9':
+//       result.unshift('XC');
+//       break;
+//   }
 
-  switch (myNum[1]) {
-    case '0':
-      result.unshift('');
-      break;
-    case '1':
-      result.unshift('X');
-      break;
-    case '2':
-      result.unshift('XX');
-      break;
-    case '3':
-      result.unshift('XXX');
-      break;
-    case '4':
-      result.unshift('XL');
-      break;
-    case '5':
-      result.unshift('L');
-      break;
-    case '6':
-      result.unshift('LX');
-      break;
-    case '7':
-      result.unshift('LXX');
-      break;
-    case '8':
-      result.unshift('LXXX');
-      break;
-    case '9':
-      result.unshift('XC');
-      break;
-  }
+//   switch (myNum[2]) {
+//     case '0':
+//       result.unshift('');
+//       break;
+//     case '1':
+//       result.unshift('C');
+//       break;
+//     case '2':
+//       result.unshift('CC');
+//       break;
+//     case '3':
+//       result.unshift('CCC');
+//       break;
+//     case '4':
+//       result.unshift('CD');
+//       break;
+//     case '5':
+//       result.unshift('D');
+//       break;
+//     case '6':
+//       result.unshift('DC');
+//       break;
+//     case '7':
+//       result.unshift('DCC');
+//       break;
+//     case '8':
+//       result.unshift('DCCC');
+//       break;
+//     case '9':
+//       result.unshift('CM');
+//       break;
+//   }
 
-  switch (myNum[2]) {
-    case '0':
-      result.unshift('');
-      break;
-    case '1':
-      result.unshift('C');
-      break;
-    case '2':
-      result.unshift('CC');
-      break;
-    case '3':
-      result.unshift('CCC');
-      break;
-    case '4':
-      result.unshift('CD');
-      break;
-    case '5':
-      result.unshift('D');
-      break;
-    case '6':
-      result.unshift('DC');
-      break;
-    case '7':
-      result.unshift('DCC');
-      break;
-    case '8':
-      result.unshift('DCCC');
-      break;
-    case '9':
-      result.unshift('CM');
-      break;
-  }
+//   switch (myNum[3]) {
+//     case '0':
+//       result.unshift('');
+//       break;
+//     case '1':
+//       result.unshift('M');
+//       break;
+//     case '2':
+//       result.unshift('MM');
+//       break;
+//     case '3':
+//       result.unshift('MMM');
+//       break;
+//     case '4':
+//       result.unshift('MMMM');
+//       break;
+//   }
 
-  switch (myNum[3]) {
-    case '0':
-      result.unshift('');
-      break;
-    case '1':
-      result.unshift('M');
-      break;
-    case '2':
-      result.unshift('MM');
-      break;
-    case '3':
-      result.unshift('MMM');
-      break;
-    case '4':
-      result.unshift('MMMM');
-      break;
-  }
+//   return result.join('');
+// }
 
-  return result.join('');
-}
-
-log(convertToRoman(2014));
+// log(convertToRoman(2014));
 
 /////////////////////////// unit 3 ///////////////////////////////
+
+function rot13(str) {
+  str = str.split('');
+  function changeNum(num) {
+    if (num > 90 || num < 65) {
+      num = num;
+    }
+    else if ((num + 13) > 90) {
+      num = 65 + (12 - (90 - num));
+    } else {
+      num = num + 13;
+    }
+    return num;
+  }
+  for (let i = 0; i < str.length; i++) {
+    let myNum = str[i].codePointAt(0);
+    str[i] = String.fromCharCode(changeNum(myNum));
+  }
+  return str.join('');
+}
+
+log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
 
 /////////////////////////// unit 4 ///////////////////////////////
 
